7e875dd054f80ea0d6aa6729b659d3ec6d4ab08d
PINGUIN <124286792+MxHakon@users.noreply.github.com>
2024-09-26 11:24:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ const keyInput = document.getElementById('keyInput');
 // Add event listener for the login button
 loginButton.addEventListener('click', function() {
     const enteredKey = keyInput.value;
-    if (enteredKey === 'zaqxsw') {
+    if (enteredKey === 'qazwsxedc') {
         // Redirect to another page
         window.location.href = 'loggedin.html'; // Adjust the URL to where the logged-in page will be
     } else {
